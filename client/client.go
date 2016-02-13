beb3b35179900eff503f01a084750018e55684b2
Max <max.f.kernchen@gmail.com>
2016-02-13 14:20:58 -0500

    basic idea of metainfo Uses a map of key and values keys are strings and values are other maps "info" key points to another map which is the information directory (houses info about file, chunking, and hash values of each chunk)

--- client/client.go
+++ client/client.go
@@ -4,7 +4,36 @@ import (
 "fmt"
 "io"
 "os"
+	"strconv"
 )
+
+var metamap map[string]map[string]string
+var infodict map[string]string
+
+func metainfo(src string){
+	metamap = make(map[string]map[string]string)
+	infodict = make(map[string]string)
+
+	fileInfo, err := os.Stat(src)
+	if err != nil {
+	}
+	metamap["announce"] = map[string]string{"announceInner": "url of tracker annouce"}
+	temp := fileInfo.ModTime().String()
+	metamap["creation_date"] =  map[string]string{"creation_dateInner": temp}
+
+	infodict["length"] = strconv.FormatInt(fileInfo.Size(), 10) //covert to string
+	infodict["name"] = fileInfo.Name()
+	infodict["piece_length"] = "fileInfo.Size()/constant"
+	infodict["pieces"] = "sha1.Sum(chunckBuffer)"
+	metamap["info"] = infodict
+
+	fmt.Println(metamap["announce"])
+	fmt.Println(metamap["creation_date"])
+
+	fmt.Println(metamap["info"])
+
+
+}
 
 func fileCopy(src, dst string) error {
 	in, err := os.Open(src) // Opens input
@@ -29,7 +58,7 @@ func fileCopy(src, dst string) error {
 }
 
 func main() {
-	fmt.Println("Hello World!")
+	/*fmt.Println("Hello World!")
 	fmt.Println("Cool Beans!")
 	err := fileCopy(os.Args[1], os.Args[2])
 	
@@ -38,4 +67,7 @@ func main() {
 	} else {
 		fmt.Println(os.Args[1] + " copied to " + os.Args[2])
 	}
+	*/
+
+	metainfo(os.Args[1]);
 }
