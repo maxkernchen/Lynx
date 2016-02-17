f2abe804b6a7a651481434772605119e564784fa
Michael Bruce <mjb1337@gmail.com>
2016-02-17 18:47:26 -0500

    Added Meta Deletion And Meta Unit Tests

--- client/client.go
+++ client/client.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"strconv"
 	"path/filepath"
+	"errors"
 )
 
 type Peer struct {
@@ -17,67 +18,120 @@ type Peer struct {
 }
 
 type File struct {
-	length int
-	path string
-	name string
+	length       int
+	path         string
+	name         string
+	pieces       string
 	piece_length int
-	pieces string
 }
+
 var files []File
 var trackerIP string // Will be set after parsing Metainfo
 var peers []Peer
 
-const END_OF_FILE = ":#!"
+const END_OF_ENTRY = ":#!"
+const META_VALUE_INDEX = 1
 
+func deleteEntry(nameToDelete string) {
 
+	i := 0
+	for i < len(files){
+		if(nameToDelete == files[i].name){
+			if len(files) > 2{
+				files = append(files[:i], files[i+1:]...)
+			}else if(i == 0){
+				files = append(files[i:])
+			}else if(i == 1){
+				files = append(files[:i])
+			}
+		}
+		i++
+	}
+}
 
-func metainfo(metainfo_path string)error{
-	metainfo_file, err := os.Open(metainfo_path)
+func updateMetainfo() error {
+
+	err := os.Remove("meta.info")
 	if err != nil{
+		fmt.Print(err)
 		return err
+	}
+
+	newMetainfo, err := os.Create("meta.info")
+	if err != nil {
+		//fmt.Println("hi")
+		return err
+	}
+
+	i := 0
+	newMetainfo.WriteString("annouce:::" + trackerIP + "\n")
+	for i < len(files) {
+
+		newMetainfo.WriteString("length:::" + strconv.Itoa(files[i].length) +"\n")
+		newMetainfo.WriteString("path:::" + files[i].path + "\n")
+		newMetainfo.WriteString("name:::" + files[i].name + "\n")
+		newMetainfo.WriteString("pieces_length:::" + strconv.Itoa(files[i].piece_length) + "\n")
+		newMetainfo.WriteString("pieces:::" + files[i].pieces + "\n")
+		newMetainfo.WriteString(END_OF_ENTRY + "\n")
+		i++
+
+	}
+
+	return newMetainfo.Close()
+
+}
+
+func parseMetainfo(metainfo_path string) error {
+	metainfo_file, err := os.Open(metainfo_path)
+	if err != nil {
+		return err
+	} else if metainfo_path != "meta.info" {
+		return errors.New("Invalid File Type")
 	}
 
 	scanner := bufio.NewScanner(metainfo_file)
 	temp_file := File{}
 
 	for scanner.Scan() {
-		temp_line := scanner.Text()
-		split_arry := strings.Split(temp_line, ":::")
 
-		if(strings.Contains(temp_line,"announce")) {
-			trackerIP = split_arry[1]
-		}else if(strings.Contains(temp_line,"piece_length")){
-			temp_int,err := strconv.Atoi(split_arry[1])
+		line  := strings.TrimSpace(scanner.Text())
+		split := strings.Split(line, ":::")
+
+		if split[0] == "announce" {
+			trackerIP = split[META_VALUE_INDEX]
+		} else if split[0] == "pieces_length" {
+			temp_int, err := strconv.Atoi(split[META_VALUE_INDEX])
 			temp_file.piece_length = temp_int
-			if err != nil{
+			if err != nil {
 				return err
 			}
-		}else if(strings.Contains(temp_line, "length")){
-			temp_int,err := strconv.Atoi(split_arry[1])
+		} else if split[0] == "length" {
+			temp_int, err := strconv.Atoi(split[META_VALUE_INDEX])
 			temp_file.length = temp_int
-			if err != nil{
+			if err != nil {
 				return err
 			}
-		}else if(strings.Contains(temp_line, "path")){
-			temp_file.path = split_arry[1]
-		}else if(strings.Contains(temp_line,"name")){
-			temp_file.name = split_arry[1]
-		}else if(strings.Contains(temp_line,"pieces")){
-			temp_file.pieces = split_arry[1]
-		}else if(strings.Contains(temp_line,END_OF_FILE)){
+		} else if (strings.Contains(line, "path")) {
+			temp_file.path = split[META_VALUE_INDEX]
+		} else if (strings.Contains(line, "name")) {
+			temp_file.name = split[META_VALUE_INDEX]
+		} else if (strings.Contains(line, "pieces")) {
+			temp_file.pieces = split[META_VALUE_INDEX]
+		} else if (strings.Contains(line, END_OF_ENTRY)) {
 			files = append(files, temp_file)  //if there is a duplicate whole thing goes empty
 			temp_file = File{}
 		}
 
 	}
-	fmt.Printf("%v", files)
+
+	//fmt.Printf("%v", files)
 
 	return metainfo_file.Close()
 }
 
-func add_to_metainfo(path_add, path_metainfo string) error{
+func addToMetainfo(path_add, path_metainfo string) error {
 	//adding_file, err := os.Open(path_add)
-	metainfo_file, err := os.OpenFile(path_metainfo, os.O_APPEND|os.O_WRONLY, 0644)
+	metainfo_file, err := os.OpenFile(path_metainfo, os.O_APPEND | os.O_WRONLY, 0644)
 																		//appends to metainfo
 																		// needs permissions?
 	if err != nil{
@@ -88,6 +142,17 @@ func add_to_metainfo(path_add, path_metainfo string) error{
 	if err != nil{
 		return err
 	}
+
+	parseMetainfo(path_metainfo)
+	i := 0
+	for i < len(files) {
+		if files[i].name == add_info.Name() {
+			//fmt.Println(files[i].name)
+			return errors.New("Can't Add Duplicates To Metainfo")
+		}
+		i++
+	}
+
 	temp_size := add_info.Size() 			//write length
 	temp_str := strconv.FormatInt(temp_size,10)
 	metainfo_file.WriteString("length:::" + temp_str + "\n")
@@ -99,19 +164,11 @@ func add_to_metainfo(path_add, path_metainfo string) error{
 
 	metainfo_file.WriteString("path:::" + temp_file_path + "\n")
 	metainfo_file.WriteString("name:::" + add_info.Name() + "\n")
-	metainfo_file.WriteString("pieces_length:::-1 \n")
-	metainfo_file.WriteString("pieces:::chuncking not currently implemented \n")
-	metainfo_file.WriteString(END_OF_FILE +"\n")
-
-
-
-
-
-
+	metainfo_file.WriteString("pieces_length:::-1\n")
+	metainfo_file.WriteString("pieces:::chuncking not currently implemented\n")
+	metainfo_file.WriteString(END_OF_ENTRY + "\n")
 
 	return metainfo_file.Close()
-
-
 }
 
 func fileCopy(src, dst string) error {
@@ -149,8 +206,15 @@ func main() {
 		fmt.Println(os.Args[1] + " copied to " + os.Args[2])
 	}*/
 
-
-	metainfo(os.Args[2])
+	//parseMetainfo(os.Args[1])
+	parseMetainfo("meta.info")
+	i := 0
+	for i < len(files) {
+		fmt.Println(files[i])
+		if files[i].name == "test.txt" {
+		}
+		i++
+	}
 }
 
 // ------------------------- CODE BELOW THIS LINE IS UNTESTED AND DANGEROUS ------------------------- \\
@@ -174,23 +238,23 @@ func askTrackerForPeers() {
 
 }
 
-func getFile(fileID string) {
+func getFile(fileName string) {
 
 	i := 0
-	for i < len(peers) {
+	gotFile := false
+	for i < len(peers) && !gotFile {
 		conn, err := net.Dial("tcp", peers[i].IP);
 		if err != nil {
 			return
 		}
 
-		fmt.Fprintf(conn, "Do_You_Have_FileID:" + fileID)
+		fmt.Fprintf(conn, "Do_You_Have_FileName:" + fileName)
 
 		reply, err := bufio.NewReader(conn).ReadString('\n') // Waits for a String ending in newline
 
-		if reply == "NO" || err != nil {
-			break // could set boolean instead
-		} else {
-			file, err := os.Create(fileID) // Should use name instead of id
+		// Has file and no errors
+		if reply != "NO" && err == nil {
+			file, err := os.Create(fileName)
 			if err != nil {
 				break // could set boolean instead
 			}
@@ -201,9 +265,9 @@ func getFile(fileID string) {
 				break // could set boolean instead
 			}
 			fmt.Println(n, "this was sent")
+			gotFile = true
 		}
-
-
+		i++
 	}
 
 }
--- client/client_test.go
+++ client/client_test.go
@@ -30,3 +30,101 @@ func TestFileCopy(t *testing.T) {
 		t.Error("Test failed, expected failure due to permissions on nopermission.txt. Got ", result)
 	}
 }
+
+func TestAddToMetainfo(t *testing.T) {
+	parseMetainfo("meta.info")
+	hasTest := false
+
+	i := 0
+	for i < len(files) {
+		//fmt.Print(files[i].name)
+		if files[i].name == "test.txt" {
+			//fmt.Print(files[i].name)
+			hasTest = true
+		}
+		i++
+	}
+
+	result := addToMetainfo("test.txt", "meta.info")
+
+	if result != nil && !hasTest {
+		t.Error("Test failed, expected no errors. Got ", result)
+	}
+
+	parseMetainfo("meta.info")
+
+	i = 0
+	for i < len(files) {
+		if files[i].name == "test.txt" {
+			hasTest = true
+		}
+		i++
+	}
+
+	if !hasTest {
+		t.Error("test.txt was not added to metainfo.")
+	}
+
+	result = addToMetainfo("test.txt", "meta.info")
+
+	if result == nil {
+		t.Error("Test failed, expected failure due to duplicates. Got ", result)
+	}
+}
+
+func TestParseMetainfo(t *testing.T) {
+	result := parseMetainfo("fake")
+
+	if result == nil {
+		t.Error("Test failed, expected failure due non-existent file. Got ", result)
+	}
+
+	result = parseMetainfo("test.txt")
+
+	if result == nil {
+		t.Error("Test failed, expected failure due incorrect file. Got ", result)
+	}
+
+
+	result = parseMetainfo("meta.info")
+
+	if result != nil {
+		t.Error("Test failed, expected no errors. Got ", result)
+	}
+}
+
+func TestUpdateMetainfo(t *testing.T) {
+	parseMetainfo("meta.info")
+
+	result := updateMetainfo()
+
+	if result != nil {
+		t.Error("Test failed, expected no errors. Got ", result)
+	}
+}
+
+func TestDeleteEntry(t *testing.T) {
+
+	parseMetainfo("meta.info")
+
+	deleteEntry("test.txt")
+
+	i := 0
+	for i < len(files) {
+		if files[i].name == "test.txt" {
+			t.Error("Error, test.txt is still in files")
+		}
+		i++
+	}
+
+	deleteEntry("test11.txt")
+
+	i = 0
+	for i < len(files) {
+		if files[i].name == "test11.txt" {
+			t.Error("Error, test.txt is still in files")
+		}
+		i++
+	}
+
+}
