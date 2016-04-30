228f03255d0b18fd328f292d5c7614a611c3bd82
Michael Bruce <mjb1337@gmail.com>
2016-04-30 18:54:23 -0400

    Removed resources, trackDriver fix

--- trackDriver/trackDriver.go
+++ trackDriver/trackDriver.go
@@ -1,21 +1,12 @@
-/**
- *
- *	 This is just a simple driver for our tracker. Only calls Listen()
- *
- *	 @author: Michael Bruce
- *	 @author: Max Kernchen
- *
- *	 @verison: 2/17/2016
- */
-
+// This is just a simple driver for our tracker. Only calls Listen()
+// @author: Michael Bruce
+// @author: Max Kernchen
+// @verison: 2/17/2016
 package main
 
 import "capstone/tracker"
 
-/**
-
- * Function used to drive and test our server's functions
- */
+// Function used to drive and test our tracker's functions
 func main() {
 	tracker.Listen()
 }
